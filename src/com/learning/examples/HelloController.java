3d837b924d66a3d0a6d689ed57b61c9178e1760f
Jitender Kumar <jitu960@gmail.com>
2017-08-13 02:47:50 +0530

    Annotation based controller is added

--- src/com/learning/examples/HelloController.java
+++ src/com/learning/examples/HelloController.java
@@ -1,16 +1,27 @@
 package com.learning.examples;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController {
+//public class HelloController extends AbstractController {
+//
+//	@Override
+//	protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws Exception {
+//		ModelAndView mNv = new ModelAndView("hello.jsp");
+//		mNv.addObject("key", "value");
+//		return mNv;
+//	}
+//
+//}
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws Exception {
+/******************************* Annotation based controller ******************************/
+@Controller
+public class HelloController {
+
+	@RequestMapping("/Welcome")
+	protected ModelAndView greeting() {
 		ModelAndView mNv = new ModelAndView("hello");
-		mNv.addObject("key", "value");
+		mNv.addObject("key", "Welcome Back!!!");
 		return mNv;
 	}
 
